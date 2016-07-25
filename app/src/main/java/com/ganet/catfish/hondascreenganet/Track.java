5e6ee927cd02adb8659d8bba2d67a81a4415243a
OlegStepanenko_noute <catfishkin@gmail.com>
2016-07-25 22:26:07 +0300

    TestPush from Android studio

--- app/src/main/java/com/ganet/catfish/hondascreenganet/Track.java
+++ app/src/main/java/com/ganet/catfish/hondascreenganet/Track.java
@@ -8,5 +8,12 @@ public class Track {
     int folderId;
     int subFolderId;
     int trackId;
+    boolean selectStatus;
+    String trackName;
+    boolean NameB1, NameB2, NameB3, NameB4;
+    int NameBlocks;
 
+    boolean isNameComplete() {
+        return true;
+    }
 }
